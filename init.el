d21495e2ab1820cc798a11c52596e76030477d20
Mmalo Nicolas <malo.nicolas-coqueron@grenoble-inp.org>
2025-07-30 03:11:02 +0200

    init config

---
+++ init.el
@@ -0,0 +1,20 @@
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons-dired anzu auctex centaur-tabs copilot dap-mode
+			 dashboard doom-themes elpy flycheck glsl-mode
+			 helm-xref irony jupyter minimap nerd-icons
+			 treemacs-all-the-icons treemacs-icons-dired
+			 treemacs-magit treemacs-nerd-icons
+			 treemacs-projectile use-package vterm
+			 which-key)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
